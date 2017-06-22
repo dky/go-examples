89806da4b9d1ccfc12eb3bdb0ce2943bc23f0de6
Don Ky <don.d.ky@gmail.com>
2017-06-22 10:08:19 -0400

    Print out memory locations

--- pointers/using-pointers-function/main.go
+++ pointers/using-pointers-function/main.go
@@ -4,10 +4,12 @@ import "fmt"
 
 func zero(z *int) {
 	*z = 0
+	fmt.Println("z address in zero:", z)
 }
 
 func main() {
 	x := 10
 	zero(&x)
+	fmt.Println("x address in main:", &x)
 	fmt.Println(x) //x is 0 since we are passing a memory location to the func
 }
