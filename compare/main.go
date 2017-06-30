9bda0134f26039392e1a0032dfa38e5210f076e4
Don Ky <don.d.ky@gmail.com>
2017-06-30 12:52:48 -0400

    Compare function

---
+++ compare/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func compare(i1, i2 int) (bool, int) {
+	if i1 > i2 {
+		return false, i1 - i2
+	} else if i2 > i1 {
+		return false, i2 - i1
+	} 
+	return true, 0
+}
+
+func main() {
+	fmt.Println(compare(1, 1))
+}
