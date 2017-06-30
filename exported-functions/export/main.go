8321ed38381135387054eca30ad0cdb597998e19
Don Ky <don.d.ky@gmail.com>
2017-06-30 09:58:43 -0400

    Add exported functions

---
+++ exported-functions/export/main.go
@@ -0,0 +1,10 @@
+package export
+
+//import "fmt"
+
+func PrintMe(i string) string {
+	//fmt.Println(i)
+	return i
+}
+
+
---
+++ exported-functions/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"./export"
+)
+
+func main() {
+	fmt.Println("vim-go")
+	fmt.Println(export.PrintMe("Hello you"))
+}
