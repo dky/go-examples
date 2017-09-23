ff647c04c0f078cc4de57cfcfdf9a25970f3b882
Don Ky <don.d.ky@gmail.com>
2017-09-22 21:14:23 -0400

    Add semi-working binary tree example

--- algorithms/binary-search/main.go
+++ algorithms/binary-search/main.go
@@ -1,10 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	numberList := []int{1, 3, 5, 7, 9, 1, 3, 5, 9}
+	binarySearch(numberList, 3)
 }
 
-func half(i int) int {
+func binarySearch(list []int, item int) {
+	low := 0
+	high := len(list) - 1
+	mid := low + high / 2
 
+	guess := list[mid]
+
+	fmt.Printf("first pass: low: %d, high: %d, mid: %d\n", low, high, mid)
+	fmt.Println("item is:", item)
+	fmt.Println("guess is:", guess)
+
+	for _,i := range list {
+		if guess == item {
+			fmt.Println("found!")
+			os.Exit(3)
+			fmt.Println(mid)
+		} else if (guess < item) {
+			//too low
+			low = mid + 1
+			fmt.Println("guess < item, new low is:", low)
+			os.Exit(3)
+		} else if (guess > item) {
+			//too high
+			high = mid - 1
+			fmt.Println("guess > item, new high is:", high)
+			os.Exit(3)
+		}
+		fmt.Println(i)
+	}
+}
