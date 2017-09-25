6f036b4a8d07480cc53ece93942ef8607bdf25ef
Don Ky <don.d.ky@gmail.com>
2017-09-25 19:49:45 -0400

    Adding more detail to bin search

--- algorithms/binary-search/2/main.go
+++ algorithms/binary-search/2/main.go
@@ -2,27 +2,33 @@ package main
 
 import (
 	"fmt"
+//	"os"
 )
 
-func BinarySearch(target_map []int, value int) int {
+//log2n. In this case it's 16, 2 * 2 = 4 * 2 = 8 * 2 = 16
+//log2n = 4
 
+func BinarySearch(search_slice []int, value int) int {
 	low := 0
-	high := len(target_map) - 1
+	high := len(search_slice) - 1
+	fmt.Println("high:", high)
 
 	for low <= high {
-
 		median := (low + high) / 2
-		fmt.Println(median)
+		fmt.Println("med:", median)
 
-		if target_map[median] < value {
+		if search_slice[median] < value {
 			low = median + 1
+			fmt.Println("new low:", low)
+		//median is 7, is this lower than our search for value of 15?
 		} else {
 			high = median - 1
+			fmt.Println("new high:", high)
 		}
 
 	}
 
-	if low == len(target_map) || target_map[low] != value {
+	if low == len(search_slice) || search_slice[low] != value {
 		return -1
 	} else {
 		return low
@@ -31,7 +37,6 @@ func BinarySearch(target_map []int, value int) int {
 }
 
 func main() {
-	//16, 8, 4, 2, 1
-	values := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50}
+	values := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
 	fmt.Println(BinarySearch(values, 15))
 }
